90b3add7e2cdad5af4e07d05f9fdfb8b34cddcdb
Shawn Morford <DarkMorford@Gmail.com>
2016-10-27 20:26:29 -0700

    Add "default" onCommand() handler for unimplemented commands.

--- src/main/java/net/darkmorford/lrrcraftbukkit/LRRcraftBukkit.java
+++ src/main/java/net/darkmorford/lrrcraftbukkit/LRRcraftBukkit.java
@@ -1,12 +1,33 @@
 package net.darkmorford.lrrcraftbukkit;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class LRRcraftBukkit extends JavaPlugin {
+
+    Logger logger;
+
     @Override
     public void onEnable() {
+        // Get the logger
+        logger = getLogger();
+
         // Connect the refresh command to its implementation
-        getCommand("lrr refresh").setExecutor(new RefreshCommand());
+        logger.info("Registering commands");
+//      getCommand("lrrcraftbukkit").setExecutor(new RefreshCommand());
+
+//      logger.info("Registering recipes");
+//      Recipe leatherRecipe = new FurnaceRecipe(new ItemStack(Material.LEATHER), Material.ROTTEN_FLESH);
+//      Bukkit.addRecipe(leatherRecipe);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
+        sender.sendMessage("That command is not yet implemented.");
+        return true;
     }
 
     @Override

e1dccee3c54868a4410b03f59b661d77a207ae7a
DarkMorford <DarkMorford@users.noreply.github.com>
2016-11-01 09:47:21 -0700

    Add an event handler for the player changing worlds.

--- src/main/java/net/darkmorford/lrrcraftbukkit/Listeners/PlayerJoinListener.java
+++ src/main/java/net/darkmorford/lrrcraftbukkit/Listeners/PlayerJoinListener.java
@@ -4,6 +4,7 @@ import net.darkmorford.lrrcraftbukkit.LRRcraftBukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener {
@@ -14,6 +15,16 @@ public class PlayerJoinListener implements Listener {
         String name = player.getDisplayName();
 
         LRRcraftBukkit.logger.info(name + " has joined the server.");
+    }
+
+    @EventHandler
+    public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
+        Player player = event.getPlayer();
+        String playerName = player.getDisplayName();
+        String fromWorld = event.getFrom().getName();
+        String toWorld = player.getWorld().getName();
+
+        LRRcraftBukkit.logger.info(playerName + " has just gone from " + fromWorld + " to " + toWorld);
     }
 
 }

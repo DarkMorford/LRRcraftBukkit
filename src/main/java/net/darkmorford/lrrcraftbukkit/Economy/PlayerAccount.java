da13ad771d78b8d635da7d7516ad0008f4d294e8
DarkMorford <DarkMorford@users.noreply.github.com>
2016-11-01 21:29:24 -0700

    Clean up PlayerAccount class.

--- src/main/java/net/darkmorford/lrrcraftbukkit/Economy/PlayerAccount.java
+++ src/main/java/net/darkmorford/lrrcraftbukkit/Economy/PlayerAccount.java
@@ -1,75 +1,40 @@
 package net.darkmorford.lrrcraftbukkit.Economy;
 
-import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-public class PlayerAccount implements ConfigurationSerializable {
+public class PlayerAccount {
     // Member variables
-    private UUID   playerId;
-    private String playerName;
-    private int    playerBalance;
+    private Player _player;
+    private double _balance;
 
     // Constructors
-    PlayerAccount(Player player, int balance) {
-        playerId      = player.getUniqueId();
-        playerName    = player.getName();
-        playerBalance = balance;
-    }
-
-    PlayerAccount(UUID id, String name, int balance) {
-        playerId      = id;
-        playerName    = name;
-        playerBalance = balance;
-    }
-
-    PlayerAccount(Map<String, Object> account) {
-        playerId      = (UUID)   account.get("ID");
-        playerName    = (String) account.get("Name");
-        playerBalance = (int)    account.get("Balance");
+    PlayerAccount(Player player, double balance) {
+        _player = player;
+        _balance = balance;
     }
 
     // Accessors
-    UUID getId() {
-        return playerId;
+    Player getPlayer() {
+        return _player;
     }
 
-    String getName() {
-        return playerName;
-    }
-
-    int getBalance() {
-        return playerBalance;
+    double getBalance() {
+        return _balance;
     }
 
     // Mutators
-    int setBalance(int amount) {
-        playerBalance = amount;
-        return playerBalance;
+    double setBalance(double amount) {
+        _balance = amount;
+        return _balance;
     }
 
-    int addBalance(int amount) {
-        playerBalance += amount;
-        return playerBalance;
+    double addBalance(double amount) {
+        _balance += amount;
+        return _balance;
     }
 
-    int subtractBalance(int amount) {
-        playerBalance -= amount;
-        return playerBalance;
-    }
-
-    // Serialization
-    @Override
-    public Map<String, Object> serialize() {
-        Map<String, Object> serialized = new HashMap<>();
-
-        serialized.put("ID", playerId);
-        serialized.put("Name", playerName);
-        serialized.put("Balance", playerBalance);
-
-        return serialized;
+    double subtractBalance(double amount) {
+        _balance -= amount;
+        return _balance;
     }
 }
